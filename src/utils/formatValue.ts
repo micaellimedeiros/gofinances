53824b19421fb50f64e02a9039f52252b051faf9
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-05-22 10:54:21 -0300

    added: format value to pt-br

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,7 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
 
 export default formatValue;
